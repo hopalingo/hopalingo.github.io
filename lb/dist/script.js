80d2683020ea06e593f06f0f36178d7fba154ec2
jairbound <158995037+jairbound@users.noreply.github.com>
2024-08-03 17:23:40 +1000

    Update script.js

--- lb/dist/script.js
+++ lb/dist/script.js
@@ -1,30 +1,17 @@
 const data = {
-  title: 'Leaderboard',
+  title: 'Hopalingo Streak Leaderboard',
   people: [
   {
-    name: 'John Hancock',
+    name: 'Ezra',
     image: 'http://lorempixel.com/100/100',
     score: 2000 },
 
   {
-    name: 'Sebastian Lofgren',
+    name: 'Noah',
     image: 'http://lorempixel.com/120/120',
     score: 2050 },
 
-  {
-    name: 'Donald Trump',
-    image: 'http://lorempixel.com/110/110',
-    score: 51 },
-
-  {
-    name: 'Bill Hader',
-    image: 'http://lorempixel.com/105/105',
-    score: 2103 },
-
-  {
-    name: 'Salvador Dali',
-    image: 'http://lorempixel.com/101/101',
-    score: 2019 }] };
+  };
 
 
 
