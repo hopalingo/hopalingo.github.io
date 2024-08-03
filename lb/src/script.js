065343398177920f0c23cfc0c7720e8d4185bb50
jairbound <158995037+jairbound@users.noreply.github.com>
2024-08-03 17:28:22 +1000

    Update script.js

--- lb/src/script.js
+++ lb/src/script.js
@@ -2,29 +2,19 @@ const data = {
 	title: 'Leaderboard',
 	people: [
 		{
-			name: 'John Hancock',
+			name: 'alex',
 			image: 'http://lorempixel.com/100/100',
-			score: 2000
+			score: 45
 		},
 		{
-			name: 'Sebastian Lofgren',
+			name: 'noah',
 			image: 'http://lorempixel.com/120/120',
-			score: 2050
+			score: 1
 		},
 		{
-			name: 'Donald Trump',
-			image: 'http://lorempixel.com/110/110',
-			score: 51
-		},
-		{
-			name: 'Bill Hader',
-			image: 'http://lorempixel.com/105/105',
-			score: 2103
-		},
-		{
-			name: 'Salvador Dali',
+			name: 'ezra',
 			image: 'http://lorempixel.com/101/101',
-			score: 2019
+			score: 2
 		}
 	]
 }
